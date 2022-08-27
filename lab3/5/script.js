dadc94ee320663fe7c8256a42b59da17e22ac787
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-08-27 14:21:52 +0700

    lab3/5 update

---
+++ lab3/5/script.js
@@ -0,0 +1,10 @@
+function OnClick(){
+    let name = document.getElementById("Name")
+    let phone = document.getElementById("Phone")
+    let mail = document.getElementById("Email")
+    let company = document.getElementById("company")
+    document.getElementById("cardname").innerHTML = name
+    document.getElementById("cardphone").innerHTML = phone
+    document.getElementById("cardmail").innerHTML = mail
+    document.getElementById("cardcompany").innerHTML = company
+}
