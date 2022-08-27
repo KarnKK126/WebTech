50892636e999f90c5a6cb846a73e4bedea0b8154
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-08-27 15:34:30 +0700

    update

--- lab3/5/script.js
+++ lab3/5/script.js
@@ -1,8 +1,8 @@
-function OnClick(){
-    let name = document.getElementById("Name")
-    let phone = document.getElementById("Phone")
-    let mail = document.getElementById("Email")
-    let company = document.getElementById("company")
+function change(){
+    let name = document.getElementById("Name").value
+    let phone = document.getElementById("Phone").value
+    let mail = document.getElementById("Email").value
+    let company = document.getElementById("company").value
     document.getElementById("cardname").innerHTML = name
     document.getElementById("cardphone").innerHTML = phone
     document.getElementById("cardmail").innerHTML = mail
