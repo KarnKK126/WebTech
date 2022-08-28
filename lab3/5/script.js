1d835f7d4d3a61ce0a05b8dcfcbe97b4795b08c8
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-08-28 11:01:27 +0700

    lab3/5 update

--- lab3/5/script.js
+++ lab3/5/script.js
@@ -3,8 +3,18 @@ function change(){
     let phone = document.getElementById("Phone").value
     let mail = document.getElementById("Email").value
     let company = document.getElementById("company").value
-    document.getElementById("cardname").innerHTML = name
-    document.getElementById("cardphone").innerHTML = phone
-    document.getElementById("cardmail").innerHTML = mail
-    document.getElementById("cardcompany").innerHTML = company
+    if (name.length < 1){
+        alert('Fullname can\'t be empty');
+    } else if (phone.length < 1){
+        alert('Phone number can\'t be empty');
+    } else if (mail.length < 1){
+        alert('Email can\'t be empty');
+    } else if (company.length < 1){
+        alert('Company name can\'t be empty');
+    } else {
+        document.getElementById("cardname").innerHTML = name
+        document.getElementById("cardphone").innerHTML = phone
+        document.getElementById("cardmail").innerHTML = mail
+        document.getElementById("cardcompany").innerHTML = company
+    }
 }
