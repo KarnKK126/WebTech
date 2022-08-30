20002a03f9cd0a93eed34efd00baf2db0105d6c2
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-08-31 01:05:13 +0700

    update webtech

---
+++ lab4/profile/script.js
@@ -0,0 +1,11 @@
+
+function changetokid(){
+document.getElementById("profilepic").src="./img/Karnkid.jpg";
+}
+function changetoschool(){
+    document.getElementById("profilepic").src="./img/Karnschool.jpg";
+}
+function changetouni(){
+    document.getElementById("profilepic").src="./img/KarnUni.jpg";
+}
+// var school = "src(./img)"
